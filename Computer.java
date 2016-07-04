03a9b43d76d0252011fde4460e9aeeafd4704760
Douglas Walsh <douglaswalsh3@gmail.com>
2016-07-04 15:30:18 -0400

    working on one player version

---
+++ Computer.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.HashMap;
+
+public class Computer {
+	String computerWord;
+	HashMap <Integer, String> dictionary = new HashMap <Integer, String>();
+
+	void createDictionary(){
+	dictionary.put(1,"gladiator");
+	dictionary.put(2,"elephant");
+	dictionary.put(3,"crocodile");
+	dictionary.put(4,"balloon");
+	dictionary.put(5,"history");
+	dictionary.put(6,"jellyfish");
+	dictionary.put(7,"bear");
+	dictionary.put(8,"potatoes");
+	dictionary.put(9,"giraffe");
+	dictionary.put(10,"mannequin");	
+	}
+
+	//System.out.println(dictionary);
+
+	void computerSetWord() {
+		Random rn = new Random();
+		int randomNum = rn.nextInt((10) + 1);
+		computerWord = dictionary.get(randomNum);
+		System.out.println("the computer has chosen a word");
+		System.out.println(computerWord);
+	}
+
+
+
+}
--- GameLauncher.java
+++ GameLauncher.java
@@ -2,8 +2,13 @@ public class GameLauncher {
 	public static void main(String[] args) {
 		Player p1 = new Player();
 		Player p2 = new Player();
+		Computer c = new Computer();
 		gameHelper h = new gameHelper();
+		
 
+		h.oneOrTwoPlayers();
+
+		if (h.onePlayer == false) {
 		p1.setName();
 		p1.sayWelcome();
 
@@ -13,7 +18,20 @@ public class GameLauncher {
 		h.setTargetWord();
 		h.setAnswer();
 		h.makeGuess();
-		h.endGame();
+		h.endGame();			
+		} else {
+			c.createDictionary();
+			c.computerSetWord();
+			h.setTargetWord();
+			System.out.println(h.targetWord2);
+			System.out.println(h.guessList);
+			System.out.println(h.answer);
+			//System.out.println(c.computerWord);
+			//h.makeGuess();
+			//h.endGame();
+		}
+
+
 		
 	}
 }
